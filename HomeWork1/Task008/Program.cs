162319b2e1bc80e4fefddd3bf8fdd785a801fd7c
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-22 17:19:54 +0700

    add Task008

---
+++ HomeWork1/Task008/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int count = 1;
+
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + A);
+while (count <= A)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+    }
+    count++;
+}
+if (A <= 1)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
+
+
+
+
