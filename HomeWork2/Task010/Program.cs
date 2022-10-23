0389c1f36bb2cf83f34ac49188a6fbf8c3ffec58
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-23 17:33:27 +0700

    I made a task010

---
+++ HomeWork2/Task010/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+void FindingTheSecondDigit()
+{
+    Console.WriteLine("Введите любое трёхзначное число:");
+    int numA = Convert.ToInt32(Console.ReadLine());
+    if (numA >= -999 && numA <= 999) // защита от пользователя который не вводт 3-х значное число!
+    {
+        int dozens = (numA % 100) / 10;
+        Console.WriteLine("Вторая цифра в числе " + numA + ", это " + dozens);
+    }
+    else
+    {
+        Console.WriteLine("Введено не трёхзначное число!Введите трёхзначное число!");
+        FindingTheSecondDigit();
+    }
+}
+
+FindingTheSecondDigit();
