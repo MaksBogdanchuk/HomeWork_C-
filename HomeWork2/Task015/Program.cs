21a3a630ecf8bb17a5218d77b98f23b3442bec49
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-23 19:11:24 +0700

    I made a task015

---
+++ HomeWork2/Task015/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите день недели(от 1 до 7):");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+
+void DayOfTheWeekWeekend()
+{
+    if (numA >= 1 && numA <= 5)
+    {
+        Console.WriteLine(numA + " день недели - не выходной!");
+    }
+    else if (numA >= 6 && numA <= 7)
+    {
+        Console.WriteLine(numA + " день недели - выходной!");
+    }
+    else
+    {
+        Console.WriteLine("В неделе 7 дней!");
+    }
+}
+
+DayOfTheWeekWeekend();
