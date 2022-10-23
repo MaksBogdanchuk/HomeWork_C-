aee4655cf7f15a77f010544113eb82d315f22062
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-23 18:55:53 +0700

    I made a task013

---
+++ HomeWork2/Task013/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите любое число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+
+void FindingTheThirdDigit()
+{
+    if (numA >= -100 && numA >= 100) // защита от пользователя который вводит 2-ухзначное число!
+    {
+        while (numA >= -1000 && numA >= 1000) // не могу допереть,как сделать нахождение 3-ей цифры в отрицательном числе... Не работает с отрицательными числами.
+        {
+            numA = numA / 10;
+        }
+        int Third = numA % 10;
+        Console.WriteLine("Третья цифра в числе, это " + Third);
+    }
+
+    else
+    {
+        Console.WriteLine("В числе " + numA + " нет третьей цифры!");
+    }
+}
+
+FindingTheThirdDigit();
