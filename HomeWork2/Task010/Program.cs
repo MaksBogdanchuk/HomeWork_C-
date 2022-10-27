2a3a902fc7468d48485f847b471664d924f46daa
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-27 19:19:12 +0700

    Changing the decision Task010

--- HomeWork2/Task010/Program.cs
+++ HomeWork2/Task010/Program.cs
@@ -4,10 +4,10 @@ void FindingTheSecondDigit()
 {
     Console.WriteLine("Введите любое трёхзначное число:");
     int numA = Convert.ToInt32(Console.ReadLine());
-    if (numA >= -999 && numA <= 999) // защита от пользователя который не вводт 3-х значное число!
+    if (numA >= -999 && numA <= -100 || numA >= 100 && numA <= 999) // защита от пользователя который не вводт 3-х значное число!
     {
         int dozens = (numA % 100) / 10;
-        Console.WriteLine("Вторая цифра в числе " + numA + ", это " + dozens);
+        Console.WriteLine("Вторая цифра в числе " + numA + ", это " + Math.Abs(dozens));
     }
     else
     {
