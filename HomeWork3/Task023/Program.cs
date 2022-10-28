38631b353697b764d0e551409dff0805b0469f42
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-28 20:53:09 +0700

    I made Task023

---
+++ HomeWork3/Task023/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] MethodCube(int a)
+{
+    int i = 0;
+    int[] cube = new int[a];
+    while (i < a)
+    {
+        cube[i] = (i + 1) * (i + 1) * (i + 1);
+        i++;
+    }
+    return cube;
+}
+Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] cube = MethodCube(a);
+Console.WriteLine(string.Join(", ", cube));
+
+
+
