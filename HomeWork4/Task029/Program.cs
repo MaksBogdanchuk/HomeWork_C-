72d5145284bbf69abd77e32e124c865dd7678ba1
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-30 18:21:36 +0700

    I made of Task029

---
+++ HomeWork4/Task029/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] massiv8elements(int lenArray)
+{
+    int i = 0;
+    int[] randomArray = new int[lenArray];
+    while (i < randomArray.Length)
+    {
+        randomArray[i] = new Random().Next(1, 9);
+        i++;
+    }
+    return randomArray;
+}
+
+int lenArray = ReadInt("Введите длинну массива: ");
+int[] randomArray = massiv8elements(lenArray);
+Console.WriteLine(string.Join(", ", randomArray));
+
+
