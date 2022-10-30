0288803064a1405e27f816aaf1e9d783de5bd6a9
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-30 17:45:58 +0700

    I made of Task025

---
+++ HomeWork4/Task025/Program.cs
@@ -0,0 +1,23 @@
+﻿int ToDegree(int a, int b, int result)
+{
+    int i = 1;
+    while (i <= b)
+    {
+        result = result * a;
+        i++;
+    }
+    return result;
+}
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int numA = ReadInt("Введите число A: ");
+int numB = ReadInt("Введите число B: ");
+int result = 1;
+Console.WriteLine(ToDegree(numA, numB, result));
